fc19ece82dc072fbf0f4a4a44a5c891aa4fc41df
santiagomayorga777 <santiagomayorga777@gmail.com>
2024-08-10 17:42:57 +0000

    preguntar nombre

--- inicio.py
+++ inicio.py
@@ -5,5 +5,9 @@ if edad < 18:
   print("No puedes entrar.")
 else:
   print("Puedes entrar.")
- 
 
+ # preguntar el nombre y mostrar en pantalla
+nombre = input("Introduce tu nombre: ")
+edad = int(input("Introduce tu edad: "))
+
+print("Hola", nombre, "tienes", edad, "años.")
