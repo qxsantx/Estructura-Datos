cc8c816880f40384dde49f22a48043542076f1c4
qxsantx <135196284+qxsantx@users.noreply.github.com>
2024-08-31 16:23:38 +0000

    operacionesbasicas

--- operacionesbasicas.py
+++ operacionesbasicas.py
@@ -13,7 +13,7 @@ def operacion_basica(num1,num2,operacion):
     else:
         return "operacion no valida"
 num1=float(input("digite un numero: "))
-num2=float(input("digite otro numero: "))
 operacion=(input("que operacion desea realizar? "))
+num2=float(input("digite otro numero: "))
 resultado=operacion_basica(num1,num2,operacion)
 print("su resultado es ", resultado)
